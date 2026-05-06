a8058e65b9d5d4d35e9c2a93712c7014704aee8d
AppunAlim <affansalim06@gmail.com>
2026-05-06 13:58:18 +0700

    tambah class AppConfig

---
+++ PokeGamingStore/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeGamingStore
+{
+    internal class AppConfig
+    {
+        public int MaxCartItems { get; set; }
+        public int LowStockThreshold { get; set; }
+    }
+}
