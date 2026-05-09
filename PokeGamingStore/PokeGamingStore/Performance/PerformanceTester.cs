69d51a996a2c79a215867f1ec2b24a3de3d76b37
RifkyAL77 <rifkyalbuchorilearning@gmail.com>
2026-05-09 12:05:56 +0700

    setup project C#, implementasi katalog dengan Generics, Table-Driven, dan Performance Test

---
+++ PokeGamingStore/PokeGamingStore/Performance/PerformanceTester.cs
@@ -0,0 +1,35 @@
+﻿using PokeGamingStore.catalog;
+using PokeGamingStore.Catalog;
+using System;
+using System.Diagnostics;
+
+namespace PokeGamingStore.Performance
+{
+    public class PerformanceTester
+    {
+        public static void RunTest()
+        {
+            var catalog = new ProductCatalog<Product>();
+            Console.WriteLine("Memasukkan 100.000 data produk untuk Performance Testing...");
+
+            for (int i = 0; i < 100000; i++)
+            {
+                catalog.AddProduct(new Product
+                {
+                    Id = $"ID{i}",
+                    Name = $"GameTitle {i}",
+                    Category = (i % 2 == 0) ? "RPG" : "Action"
+                });
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var results = catalog.SearchProduct("Category", "RPG");
+
+            stopwatch.Stop();
+            Console.WriteLine($"Pencarian selesai. Ditemukan {results.Count} produk RPG.");
+            Console.WriteLine($"Waktu eksekusi pencarian: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
