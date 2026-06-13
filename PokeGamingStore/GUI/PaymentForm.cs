6dd883c775eb776f2cbf7b7cee1b9d5d85f54e2f
AppunAlim <affansalim06@gmail.com>
2026-06-13 16:29:53 +0700

    Menambahkan GUI Pembayaran dan fungsional Payment

---
+++ PokeGamingStore/GUI/PaymentForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PokeGamingStore.GUI
+{
+    internal partial class PaymentForm : Form
+    {
+        private DataTable _dtSelectedItems;
+        private decimal _totalAmount;
+
+        public PaymentForm(DataTable dtSelectedItems, decimal totalAmount)
+        {
+            _dtSelectedItems = dtSelectedItems ?? throw new ArgumentNullException(nameof(dtSelectedItems));
+            _totalAmount = totalAmount;
+
+            InitializeComponent();
+
+            // Tampilkan jumlah total tagihan dengan format rupiah
+            lblTotalTagihan.Text = $"Total Tagihan: Rp{_totalAmount:N0}";
+        }
+
+        private void BtnConfirmPayment_Click(object sender, EventArgs e)
+        {
+            string chosenMethod = rdbQris.Checked ? "QRIS" : "Transfer Bank";
+
+            string message = $"Konfirmasi Pembayaran:\n\n" +
+                             $"Total: Rp{_totalAmount:N0}\n" +
+                             $"Metode: {chosenMethod}\n\n" +
+                             $"Apakah Anda ingin melanjutkan?";
+
+            DialogResult result = MessageBox.Show(message, "Konfirmasi Pembayaran", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("Pembayaran Berhasil Dikonfirmasi!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
