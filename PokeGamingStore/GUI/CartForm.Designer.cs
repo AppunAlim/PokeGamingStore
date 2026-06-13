1e6acb7b9e132c7b7b0893f9d4151d694793f6d7
AppunAlim <affansalim06@gmail.com>
2026-06-13 16:29:13 +0700

    Menambahkan GUI dan fungsional Cart

---
+++ PokeGamingStore/GUI/CartForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PokeGamingStore.GUI
+{
+    partial class CartForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dgvCartItems = new System.Windows.Forms.DataGridView();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTotalItems = new System.Windows.Forms.Label();
+            this.lblTotalHarga = new System.Windows.Forms.Label();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCartItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCartItems
+            // 
+            this.dgvCartItems.AllowUserToAddRows = false;
+            this.dgvCartItems.AllowUserToDeleteRows = false;
+            this.dgvCartItems.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCartItems.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvCartItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCartItems.Location = new System.Drawing.Point(15, 55);
+            this.dgvCartItems.MultiSelect = false;
+            this.dgvCartItems.Name = "dgvCartItems";
+            this.dgvCartItems.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCartItems.Size = new System.Drawing.Size(650, 280);
+            this.dgvCartItems.TabIndex = 0;
+            this.dgvCartItems.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCartItems_CellContentClick);
+            this.dgvCartItems.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCartItems_CellValueChanged);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12f, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(12, 18);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(186, 21);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Keranjang Belanja Anda";
+            // 
+            // lblTotalItems
+            // 
+            this.lblTotalItems.AutoSize = true;
+            this.lblTotalItems.Location = new System.Drawing.Point(15, 350);
+            this.lblTotalItems.Name = "lblTotalItems";
+            this.lblTotalItems.Size = new System.Drawing.Size(133, 15);
+            this.lblTotalItems.TabIndex = 2;
+            this.lblTotalItems.Text = "Total Item di Keranjang: 0";
+            // 
+            // lblTotalHarga
+            // 
+            this.lblTotalHarga.AutoSize = true;
+            this.lblTotalHarga.Font = new System.Drawing.Font("Segoe UI", 10f, System.Drawing.FontStyle.Bold);
+            this.lblTotalHarga.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(175)))), ((int)(((byte)(80)))));
+            this.lblTotalHarga.Location = new System.Drawing.Point(15, 375);
+            this.lblTotalHarga.Name = "lblTotalHarga";
+            this.lblTotalHarga.Size = new System.Drawing.Size(127, 19);
+            this.lblTotalHarga.TabIndex = 3;
+            this.lblTotalHarga.Text = "Total Terpilih: Rp0";
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(175)))), ((int)(((byte)(80)))));
+            this.btnCheckout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheckout.ForeColor = System.Drawing.Color.White;
+            this.btnCheckout.Location = new System.Drawing.Point(515, 350);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(150, 45);
+            this.btnCheckout.TabIndex = 4;
+            this.btnCheckout.Text = "Lanjut Pembayaran ➜";
+            this.btnCheckout.UseVisualStyleBackColor = false;
+            this.btnCheckout.Click += new System.EventHandler(this.BtnCheckout_Click);
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Location = new System.Drawing.Point(380, 350);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(120, 45);
+            this.btnRemoveItem.TabIndex = 5;
+            this.btnRemoveItem.Text = "Hapus Item";
+            this.btnRemoveItem.UseVisualStyleBackColor = true;
+            this.btnRemoveItem.Click += new System.EventHandler(this.BtnRemoveItem_Click);
+            // 
+            // CartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7f, 15f);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 411);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.btnCheckout);
+            this.Controls.Add(this.lblTotalHarga);
+            this.Controls.Add(this.lblTotalItems);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvCartItems);
+            this.Font = new System.Drawing.Font("Segoe UI", 9f);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Keranjang Belanja - PokeGamingStore";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCartItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.DataGridView dgvCartItems;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTotalItems;
+        private System.Windows.Forms.Label lblTotalHarga;
+        private System.Windows.Forms.Button btnCheckout;
+        private System.Windows.Forms.Button btnRemoveItem;
+    }
+}
