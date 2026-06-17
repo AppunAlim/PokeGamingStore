5ae1111828a3b8f721cf0cb311f8f84655c0881e
AppunAlim <affansalim06@gmail.com>
2026-06-17 18:35:20 +0700

    Fix duplicate attribute error and update test configuration

---
+++ PokeGamingStore/PokeGamingStore.Tests/StockManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokeGamingStore;
+using System;
+
+namespace PokeGamingStore.Tests
+{
+    [TestClass]
+    public class StockManagerTests
+    {
+        [TestMethod]
+        public void ReduceStock_ValidQuantity_StockReduced()
+        {
+            StockManager manager = new StockManager();
+            Item item = new Item { Id = "I001" };
+            manager.AddCatalogItem(item, 10);
+            manager.ReduceStock("I001", 3);
+            Assert.AreEqual(7, manager.GetStock("I001"));
+        }
+    }
+}
